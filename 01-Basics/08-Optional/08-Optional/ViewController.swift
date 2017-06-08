b40333dd3b675c86f3251ebab08fcd9df71eaf67
孙金帅 <s_mike@163.com>
2017-06-08 17:41:55 +0800

    Optional

---
+++ 01-Basics/08-Optional/08-Optional/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  08-Optional
+//
+//  Created by michael on 2017/6/8.
+//  Copyright © 2017年 MYNavigationController. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+//    var name: String Class 'ViewController' has no initializers
+    var name: String? // 类型声明后添加加一个`?`操作符
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

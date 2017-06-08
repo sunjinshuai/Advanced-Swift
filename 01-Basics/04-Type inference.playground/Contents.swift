8214f096acfe1e31dbef629a012571e908381594
孙金帅 <s_mike@163.com>
2017-06-08 15:35:59 +0800

    类型推断

---
+++ 01-Basics/04-Type inference.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let meaningOfLife = 42
+// meaningOfLife 会被推测为 Int 类型
+
+// 同理，如果你没有给浮点字面量标明类型，Swift 会推断你想要的是 Double ：
+let pi = 3.14159
+// pi 会被推测为 Double 类型
